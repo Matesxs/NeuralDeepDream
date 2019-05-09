2062e5434d4d2e45aa1fe015a5521d8390ea0df7
MatesXs <martindousa186@gmail.com>
2019-05-09 02:03:45 +0200

    tweaks + data

--- dream_image.py
+++ dream_image.py
@@ -1,10 +1,11 @@
 from helpers.deepdreamer import model, load_image, recursive_optimize
 import numpy as np
 import PIL.Image
+import PIL.ImageStat
 import os
-import cv2
 import random
 from tqdm import tqdm
+import math
 
 """
 layer 1: wavy
@@ -38,6 +39,12 @@ BATCH_SIZE = 50
 TENSOR_LAYERS = model.layer_tensors[LAYER_NUMBER]
 created_count = 0
 
+def brightness(im_file):
+	im = PIL.Image.open(im_file)
+	stat = PIL.ImageStat.Stat(im)
+	r, g, b = stat.mean
+	return math.sqrt(0.241 * (r ** 2) + 0.691 * (g ** 2) + 0.068 * (b ** 2))
+
 print(f'Total frames: {VIDEO_LENGTH}')
 for i in tqdm(range(0, VIDEO_LENGTH + 1)):
 	if not os.path.isfile('dream/img_{}.jpg'.format(i + 1)):
@@ -45,11 +52,18 @@ for i in tqdm(range(0, VIDEO_LENGTH + 1)):
 
 		# this impacts how quick the "zoom" is
 		img_result = img_result[0 + X_TRIM:Y_SIZE - Y_TRIM, 0 + Y_TRIM:X_SIZE - X_TRIM]
-		img_result = cv2.resize(img_result, (X_SIZE, Y_SIZE))
+		br = brightness('dream/img_{}.jpg'.format(i))
 
-		img_result[:, :, 0] += random.choice([2, 2, 3])  # reds
-		img_result[:, :, 1] += random.choice([2, 2, 3])  # greens
-		img_result[:, :, 2] += random.choice([2, 3])  # blues
+		if 90 < br < 70:
+			# Image is too dark
+			img_result[:, :, 0] += random.choice([2, 3, 4])  # reds
+			img_result[:, :, 1] += random.choice([3, 4])  # greens
+			img_result[:, :, 2] += random.choice([3, 4])  # blues
+		else:
+			# Image is too bright
+			img_result[:, :, 0] += random.choice([2, 3])  # reds
+			img_result[:, :, 1] += random.choice([2, 3])  # greens
+			img_result[:, :, 2] += random.choice([2, 3])  # blues
 
 		img_result = np.clip(img_result, 0.0, 255.0)
 		img_result = img_result.astype(np.uint8)
@@ -58,7 +72,7 @@ for i in tqdm(range(0, VIDEO_LENGTH + 1)):
 		                                image=img_result,
 		                                num_iterations=NUM_ITERATIONS,
 		                                step_size=1.0,
-		                                rescale_factor=0.71,
+		                                rescale_factor=0.75,
 		                                num_repeats=1,
 		                                blend=0.2)
 
